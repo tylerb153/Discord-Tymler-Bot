65d9234faa3c261ca734f44b0a6ce3bbfbca0cc7
Tyler Bischoff <tbischoff2@ymail.com>
2024-03-07 02:35:21 -0500

    Slash command role add technically works

--- bot.py
+++ bot.py
@@ -25,9 +25,9 @@ async def whitelist(interaction: discord.Interaction, username: str):
     await interaction.response.send_message(finalmsg)
     await interaction.user.add_roles(discord.utils.get(interaction.user.guild.roles, name="Cult 2.0 Members"))
 
-countGroup = app_commands.Group(name="online", description='Returns whether the server is up and running or not.')
+onlineGroup = app_commands.Group(name="online", description='Returns whether the server is up and running or not.')
 
-@countGroup.command(name='status', description='Returns whether the server is up and running or not.')
+@onlineGroup.command(name='status', description='Returns whether the server is up and running or not.')
 async def status(interaction: discord.Interaction):
     url = "https://api.mcstatus.io/v2/status/java/mc.theminecraftcult.com"
     response = requests.get(url)
@@ -44,11 +44,49 @@ async def status(interaction: discord.Interaction):
         print(f"API call to {url} failed with status code {response.status_code}.")
         await interaction.response.send_message(":warning: Could not check for online status")
 
+roleGroup = app_commands.Group(name='role', description='Manages user roles')
 
+@roleGroup.command(name='add', description='adds a role with the name you select to yourself.')
+async def add(interaction: discord.Interaction, name: str):
+    customRoleDividerName = "Tylerrrrrr"
+    if name == '@everyone':
+        await interaction.response.send_message(content=f'You have @everyone by default :man_facepalming:', ephemeral=True)
+        return
+    
+    name = name.strip('@')
+
+    bannedRoles = []
+    customRoleFlag = False
+    for role in interaction.guild.roles:
+        if role.name == customRoleDividerName:
+            customRoleFlag = True
+        if customRoleFlag:
+            bannedRoles.append(role.name) 
+    if name in bannedRoles:
+            await interaction.response.send_message(content=f"I cannot give you the role **{name}** it is forbidden!", ephemeral=True)
+            print(f'Role {name} is forbidden')
+            return
+     
+    for role in interaction.guild.roles:    
+        if role.name == name:
+            if role in interaction.user.roles:
+                await interaction.response.send_message(content=f'You already have the role **{name}**.', ephemeral=True)
+                print(f'User has {name} already')
+                break
+            await interaction.user.add_roles(role)
+            await interaction.response.send_message(content=f'Added the role **{name}** to you', ephemeral=True)
+            print(f'{name} was added to user')
+            break
+        if role.name == customRoleDividerName:
+            await interaction.user.add_roles(await interaction.guild.create_role(name=name))
+            await interaction.response.send_message(content=f'Added the role **{name}** to you', ephemeral=True)
+            print(f'Created role {name} and added it to user')
+            break
 
 @client.event
 async def on_ready():
-    print(tree.add_command(countGroup))
+    print(tree.add_command(onlineGroup))
+    print(tree.add_command(roleGroup))
     await tree.sync()
     print("Ready")
 
