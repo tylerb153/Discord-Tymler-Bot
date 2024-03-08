99a596a5aee5e496adb4d59c77328c53da048b92
Tyler Bischoff <tbischoff2@ymail.com>
2024-03-08 05:54:49 -0500

    the password wasn't using the .env file

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ async def whitelist(interaction: discord.Interaction, username: str):
         command = 'ps aux'
     output = subprocess.check_output(command, shell=True).decode()
     if (os.getenv('MC_SERVER_PROCESS_NAME') in output):
-        with MCRcon(os.getenv('MINECRAFT_SERVER_IP_ADDRESS'), 'RCON_PASSWORD') as mcr: #send the whitelist command to minecraft server
+        with MCRcon(os.getenv('MINECRAFT_SERVER_IP_ADDRESS'), os.getenv('RCON_PASSWORD')) as mcr: #send the whitelist command to minecraft server
             resp = mcr.command("/whitelist add " + username)
             print(resp)
         await interaction.user.add_roles(discord.utils.get(interaction.user.guild.roles, name="Cult 2.0 Members"))
